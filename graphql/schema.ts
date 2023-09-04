dc2e6dd4c9b9f27ae572ca4a6b5e0f7c8867acdf
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2023-09-04 10:17:22 +0700

    Define User Link graphql schema

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,15 +1,5 @@
-export const typeDefs = `
-  type Link {
-    id: ID
-    title: String
-    description: String
-    url: String
-    category: String
-    imageUrl: String
-    users: [String]
-  }
+import './types/Link';
+import './types/User';
+import { builder } from './builder';
 
-  type Query {
-    links: [Link]!
-  }
-`;
+export const schema = builder.toSchema();
